5bc26fcea81edff1ccf663fc89eda879de0bec3f
Reza Ferdiansyah <rf.rezaferdiansyah@gmail.com>
2017-11-07 17:14:31 +0700

    Project eCommerce Pasar Online completed

---
+++ pasaronlinebackend/src/main/java/net/rf/pasaronlinebackend/App.java
@@ -0,0 +1,13 @@
+package net.rf.pasaronlinebackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
